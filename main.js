cfa7955b7af1cc11a3043da52b23dec570dba1d6
RaviPunia <ravipunia10@gmail.com>
2023-11-01 14:51:51 -0700

    Modify form and add js for google sheet submission

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-/*------------- SWIPER JS ---------------*/
+/*------------- Form JS ---------------*/
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxnwwRhliv8FRZsjm5oNWNGzqgBvc7NeInSW7-fFXqtmC56CM_QVDh3pIbKgEbsFBeP/exec'
+const forms = document.querySelectorAll('[name="submit-to-google-sheet"]');
+
+forms.forEach(form => {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                form.reset();
+            })
+            .catch(error => console.error('Error!', error.message));
+    });
+});
+
+
+
+/*------------- Swiper JS ---------------*/
 let swiperCards = new Swiper(".card__content", {
     loop: true,
     spaceBetween: 32,
