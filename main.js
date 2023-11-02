a854da87042c9d1712fdca8db3fc62deac5cc928
RaviPunia <ravipunia10@gmail.com>
2023-11-01 22:27:06 -0700

    Format the code with proper structure

--- main.js
+++ main.js
@@ -4,41 +4,41 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxnwwRhliv8FRZsjm5oNW
 const forms = document.querySelectorAll('[name="submit-to-google-sheet"]');
 
 forms.forEach(form => {
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-            .then(response => {
-                form.reset();
-            })
-            .catch(error => console.error('Error!', error.message));
-    });
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message));
+  });
 });
 
 
 
 /*------------- Swiper JS ---------------*/
 let swiperCards = new Swiper(".card__content", {
-    loop: true,
-    spaceBetween: 32,
-    grabCursor: true,
-  
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
     },
-  
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    968: {
+      slidesPerView: 3,
     },
-  
-    breakpoints:{
-      600: {
-        slidesPerView: 2,
-      },
-      968: {
-        slidesPerView: 3,
-      },
-    },
-  });
+  },
+});
